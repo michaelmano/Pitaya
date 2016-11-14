ecd42c5292f43c7d685ef5d970b8bc6a26680f1e
Micheal Mano <michael.mano26@gmail.com>
2016-11-14 10:04:07 +1000

    added sidebar postmeta function

--- core/pitaya-functions.php
+++ core/pitaya-functions.php
@@ -133,3 +133,34 @@ function pitaya_google_analytics() {
     echo $output;
   }
 }
+
+/*
+|--------------------------------------------------------------------------
+| Add Post Meta to all pages
+|--------------------------------------------------------------------------
+|
+| The Below function will add a post meta box to all pages that has a checkbox
+| That will enable or disable the sidebar instead of having multiple page
+| Templates I find this to be the cleanest way of doing things.
+*/
+
+
+function pitaya_add_sidebar_meta() {
+  add_meta_box('featured_checkbox_id','Pitaya Functions', 'pitaya_sidebar_meta_callback', 'page', 'side', 'high');
+}
+
+function pitaya_sidebar_meta_callback( $post ) {
+	global $post;
+	$sidebar_enabled = get_post_meta( $post->ID, 'sidebar_enabled', true );
+?>
+	<input type="checkbox" style="margin:0 16px 0 0;" name="sidebar_enabled" value="true" <?php echo (($sidebar_enabled=='true') ? 'checked="checked"': '');?>/><label for="sidebar_enabled">Enable Sidebar</span>
+<?php
+}
+
+
+function pitaya_save_sidebar_meta($post_id){
+	update_post_meta( $post_id, 'sidebar_enabled', $_POST['sidebar_enabled']);
+}
+
+add_action( 'save_post'     , 'pitaya_save_sidebar_meta');
+add_action( 'add_meta_boxes', 'pitaya_add_sidebar_meta' );
