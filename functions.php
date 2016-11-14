d41195082cb82fd09499d4f1bcb3c6cd6156f458
Micheal Mano <michael.mano26@gmail.com>
2016-11-14 14:29:49 +1000

    added removal of wp-smile and javascript from header

--- functions.php
+++ functions.php
@@ -31,6 +31,14 @@ function pitaya_scripts() {
 	wp_enqueue_script( 'pitaya-functions' );
 }
 if (!is_admin()) add_action( 'wp_enqueue_scripts', 'pitaya_scripts', 11 );
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 /*
 |--------------------------------------------------------------------------
 | Redirect Attachment Pages.
