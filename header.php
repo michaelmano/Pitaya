87275dd8935481c6109c9a05ffb925c7e0b0b31f
Micheal Mano <michael.mano26@gmail.com>
2016-11-14 14:45:40 +1000

    updated header

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
 <!doctype html>
-
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <title><?php wp_title( '|', true, 'right' ); ?></title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
+  <meta name="description" content="<?php wp_title( '|', true, 'right' ); ?>">
   <?php pitaya_favicons(); ?>
   <?php wp_head(); ?>
   <!--[if lt IE 9]>
