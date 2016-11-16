c7e2dad6e67fda1343fdc3f589acf038c1d37887
Michael Mano <michael.mano26@gmail.com>
2016-11-16 23:22:13 +1000

    fixed spacing of tabs

--- functions.php
+++ functions.php
@@ -15,9 +15,9 @@
 
 
 function pitaya_remove_wp_ver_css_js( $src ) {
-    if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
-        $src = remove_query_arg( 'ver', $src );
-    return $src;
+  if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
+    $src = remove_query_arg( 'ver', $src );
+  return $src;
 }
 add_filter( 'style_loader_src', 'pitaya_remove_wp_ver_css_js', 9999 );
 add_filter( 'script_loader_src', 'pitaya_remove_wp_ver_css_js', 9999 );
