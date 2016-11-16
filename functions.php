2ba7b901289c684cc7d7818ef4b97290abfff995
Michael Mano <michael.mano26@gmail.com>
2016-11-16 23:21:52 +1000

    fixed spacing of tabs

--- functions.php
+++ functions.php
@@ -3,23 +3,6 @@
  Functions.php - Setup of Wordpress and overrides.
 ========================================================================== */
 
-
-/*
-|--------------------------------------------------------------------------
-| Enque Pitaya Functions.
-|--------------------------------------------------------------------------
-|
-| The required files below are for the
-| activation of the theme and also
-| the theme settings page.
-*/
-require_once(dirname(__FILE__) . '/core/pitaya-activation.php');
-require_once(dirname(__FILE__) . '/core/pitaya-menu.php');
-require_once(dirname(__FILE__) . '/core/pitaya-settings.php');
-require_once(dirname(__FILE__) . '/core/pitaya-functions.php');
-require_once(dirname(__FILE__) . '/core/pitaya-post-types.php');
-
-
 /*
 |--------------------------------------------------------------------------
 | Enque Stylesheets and javascripts.
@@ -202,58 +185,75 @@ add_action('print_media_templates', function(){ ?>
 <?php });
 
 function pitaya_gallery($output, $attr) {
-    global $post;
-    if (isset($attr['orderby'])) {
-      $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
-      if (!$attr['orderby'])
-        unset($attr['orderby']);
+  global $post;
+  if (isset($attr['orderby'])) {
+    $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+    if (!$attr['orderby'])
+      unset($attr['orderby']);
+  }
+  extract(shortcode_atts([
+    'order' => 'ASC',
+    'orderby' => 'menu_order ID',
+    'type'  =>  '',
+    'id' => $post->ID,
+    'itemtag' => 'dl',
+    'icontag' => 'dt',
+    'captiontag' => 'dd',
+    'columns' => 3,
+    'size' => 'thumbnail',
+    'include' => '',
+    'exclude' => ''
+  ], $attr));
+  if ('RAND' == $order) $orderby = 'none';
+  if (!empty($include)) {
+    $include = preg_replace('/[^0-9,]+/', '', $include);
+    $_attachments = get_posts([
+      'include' => $include,
+      'post_status' => 'inherit',
+      'post_type' => 'attachment',
+      'post_mime_type' => 'image',
+      'order' => $order,
+      'orderby' => $orderby
+    ]);
+    $attachments = [];
+    foreach ($_attachments as $key => $val) {
+      $attachments[$val->ID] = $_attachments[$key];
     }
-    extract(shortcode_atts([
-      'order' => 'ASC',
-      'orderby' => 'menu_order ID',
-      'type'  =>  '',
-      'id' => $post->ID,
-      'itemtag' => 'dl',
-      'icontag' => 'dt',
-      'captiontag' => 'dd',
-      'columns' => 3,
-      'size' => 'thumbnail',
-      'include' => '',
-      'exclude' => ''
-    ], $attr));
-    if ('RAND' == $order) $orderby = 'none';
-    if (!empty($include)) {
-      $include = preg_replace('/[^0-9,]+/', '', $include);
-      $_attachments = get_posts([
-        'include' => $include,
-        'post_status' => 'inherit',
-        'post_type' => 'attachment',
-        'post_mime_type' => 'image',
-        'order' => $order,
-        'orderby' => $orderby
-      ]);
-      $attachments = [];
-      foreach ($_attachments as $key => $val) {
-        $attachments[$val->ID] = $_attachments[$key];
-      }
-    }
-    if (empty($attachments)) return '';
-      ob_start(); ?>
-      <div class="gallery gallery--<?php if ($attr['type'] === 'slider') { echo 'slider'; } else { echo 'masonry'; } ?>">
-      <?php
-      foreach ($attachments as $id => $attachment) {
-        $thumb = wp_get_attachment_image_src($id, 'medium_large');
-        $full   = wp_get_attachment_image_src($id, 'full'); ?>
-        <div class="gallery__item">
-          <a href="<?php echo $full[0]; ?>">
-            <img src="<?php echo $thumb[0]; ?>" alt="" />
-          </a>
-        </div><!-- END gallery__item -->
-      <?php } ?>
-    </div><!-- END macy -->
+  }
+  if (empty($attachments)) return '';
+    ob_start(); ?>
+    <div class="gallery gallery--<?php if ($attr['type'] === 'slider') { echo 'slider'; } else { echo 'masonry'; } ?>">
     <?php
-    $output = ob_get_contents();
-    ob_end_clean();
-    return $output;
+    foreach ($attachments as $id => $attachment) {
+      $thumb = wp_get_attachment_image_src($id, 'medium_large');
+      $full   = wp_get_attachment_image_src($id, 'full'); ?>
+      <div class="gallery__item">
+        <a href="<?php echo $full[0]; ?>">
+          <img src="<?php echo $thumb[0]; ?>" alt="" />
+        </a>
+      </div><!-- END gallery__item -->
+    <?php } ?>
+  </div><!-- END macy -->
+  <?php
+  $output = ob_get_contents();
+  ob_end_clean();
+  return $output;
 }
 add_filter('post_gallery', 'pitaya_gallery', 10, 2);
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Enque Pitaya Functions.
+|--------------------------------------------------------------------------
+|
+| The required files below are for the
+| activation of the theme and also
+| the theme settings page.
+*/
+require_once(dirname(__FILE__) . '/core/pitaya-activation.php');
+require_once(dirname(__FILE__) . '/core/pitaya-menu.php');
+require_once(dirname(__FILE__) . '/core/pitaya-settings.php');
+require_once(dirname(__FILE__) . '/core/pitaya-functions.php');
+require_once(dirname(__FILE__) . '/core/pitaya-post-types.php');
